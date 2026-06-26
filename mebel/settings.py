6c605c187102f96e14e2db4b5945f0c97f76308e
OffTheWall-13 <bubuator422@gmail.com>
2026-06-27 03:43:04 +0500

    Пересобрал индекс с новым .gitignore

--- mebel/settings.py
+++ mebel/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mebel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Loft_mebel',
+        'USER': 'mysite_user',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
